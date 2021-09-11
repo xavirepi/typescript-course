033f01910366af7ba1d5de10fb397696c8f76f62
xavirepi <xavirepi@hotmail.com>
2021-09-11 11:15:07 +0200

    lesson 3: TS basic types

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log('Hello, world');
+const isOpen: boolean = false;
+// const isOpen: boolean = string; // Throws an error.
+
+// const name: string = "Javier"; // name is a reserved word.
+// let name: string = "Javier"; // let doesn't work either.
+const myName: string = "Javier";
+
+const myAge: number = 31.04;
+
+const list: number[] = [0, 1, 2, 3];
+
+const me: [string, number, boolean] = ["Javier", 31, false]; // Type according to item position in array.
+
+const Job {
+    WebDev, 
+    WebDesigner, 
+    PM,
+};
+
+const job: Job = Job.WebDev;
+
+const phone: any = "iphone"; // Any lets pass any type through;
+const tablet: any = 3;
+
+const laptop: never = 3; // This variable won't be assignable
