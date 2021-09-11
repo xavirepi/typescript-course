74a9f8fe95c2be6696ddd8856574af37644a8f80
xavirepi <xavirepi@hotmail.com>
2021-09-11 12:18:29 +0200

    install typescript-plugin for parcel

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-const isOpen: boolean = false;
+// const isOpen: boolean = false;
 // const isOpen: boolean = string; // Throws an error.
 
 // const name: string = "Javier"; // name is a reserved word.
@@ -11,13 +11,13 @@ const list: number[] = [0, 1, 2, 3];
 
 const me: [string, number, boolean] = ["Javier", 31, false]; // Type according to item position in array.
 
-const Job {
-    WebDev, 
-    WebDesigner, 
-    PM,
-};
+// const Job {
+//     WebDev, 
+//     WebDesigner, 
+//     PM,
+// };
 
-const job: Job = Job.WebDev;
+// const job: Job = Job.WebDev;
 
 const phone: any = "iphone"; // Any lets pass any type through;
 const tablet: any = 3;
